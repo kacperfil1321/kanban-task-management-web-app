8ebb3b1af50b4e43d52ef21474e9728fe6966bc5
Kacper Filipski <filipskikacper1321@gmail.com>
2024-02-29 23:26:44 +0100

    29.02

--- js/createNewBoard.js
+++ js/createNewBoard.js
@@ -15,6 +15,7 @@ var column = document.createElement('div')
 column.classList.add('column');
 column.innerHTML = '' +
 '<input class="boardColumn" placeholder="Todo">' +
+'<span class="errorText">Can\'t be empty</span>' +
 '<svg onclick="removeColumn(this)" width="15" height="15" xmlns="http://www.w3.org/2000/svg"><g fill="#828FA3" fill-rule="evenodd"><path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z"/><path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z"/></g></svg>';
 
 // Add new column
