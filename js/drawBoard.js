0fffc3e6f355ec0a090db18aa2b564014f7021c5
Kacper Filipski <filipskikacper1321@gmail.com>
2024-03-11 22:28:23 +0100

    11.03

--- js/drawBoard.js
+++ js/drawBoard.js
@@ -5,20 +5,30 @@ function drawBoard(){
     if(localStorage.getItem('board' + localStorage.getItem('activeBoard') + 'column0')){
         var column = 0;
         while(localStorage.getItem('board' + localStorage.getItem('activeBoard') + 'column' + column)){
+            var task = 0, tasks= '';
+            while(localStorage.getItem('board' + localStorage.getItem('activeBoard') + 'column' + column + 'task' + task)){
+                var subtask = 0, subtasks = 0, subtasksComplete = 0;
+                while(localStorage.getItem('board' + localStorage.getItem('activeBoard') + 'column' + column + 'task' + task + 'subtaskStatus' + subtask)){
+                    if(localStorage.getItem('board' + localStorage.getItem('activeBoard') + 'column' + column + 'task' + task + 'subtaskStatus' + subtask) == 1){
+                        subtasksComplete++;
+                    }
+                    subtasks++;
+                }
+                tasks += '' +
+                '<div class="task">' +
+                '   <div class="taskTitle">' + localStorage.getItem('board' + localStorage.getItem('activeBoard') + 'column' + column + 'task' + task) + '</div>' +
+                '   <div class="counter">' + subtasksComplete + ' of ' + subtasks + ' subtasks</div>' +
+                '</div>';
+                task++;
+            }
             board.innerHTML += '' +
             '<div class="column">' +
-            '    <div class="title">Todo (4)</div>' +
+            '    <div class="title">Todo (' + task + ')</div>' +
             '    <div class="tasks">' +
-            '       <div class="task">' +
-            '           <div class="taskTitle">Build UI for onboarding flow</div>' +
-            '           <div class="counter">0 of 3 subtasks</div>' +
-            '       </div>' +
-            '       <div class="task">' +
-            '           <div class="taskTitle">Build UI for onboarding flow</div>' +
-            '           <div class="counter">0 of 3 subtasks</div>' +
-            '       </div>' +
+            tasks +
             '    </div>' +
             '</div>';
+            column++;
         }
     }
     else{
