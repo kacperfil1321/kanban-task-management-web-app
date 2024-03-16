37de29e2e0b7bc325b8e47cab790d2ccd937ac60
Kacper Filipski <filipskikacper1321@gmail.com>
2024-03-16 23:52:25 +0100

    16.03

--- js/addNewTask.js
+++ js/addNewTask.js
@@ -2,3 +2,14 @@ document.getElementById('addNewTask').addEventListener('click', function (){
     document.getElementById('blur').style.display = 'block';
     document.getElementById('taskWindow').style.display = 'flex';
 });
+
+document.getElementById('status').addEventListener('click', function(){
+    if(document.getElementById('statusList').style.display == 'none'){
+        document.getElementById('status').style.borderColor = '#635FC7';
+        document.getElementById('statusList').style.display = 'block';
+    }
+    else{
+        document.getElementById('status').style.borderColor = '';
+        document.getElementById('statusList').style.display = 'none';
+    }
+});
